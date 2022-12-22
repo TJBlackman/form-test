ea43fe9c2648b559268b3bb96ff00a644b680999
unknown <trevor.blackman@eclypses.com>
2022-12-21 20:15:24 -0700

    styles baby

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ function App() {
   return (
     <div className="App">
       {/* A contact form */}
+      <h1>Contact form</h1>
       <form netlify name="contact" method="POST">
         <label htmlFor="name">Name</label>
         <input type="text" id="name" name="name" />
+        <br />
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
+        <br />
         <label htmlFor="message">Message</label>
         <textarea id="message" name="message" />
+        <br />
         <button type="submit">Send</button>
       </form>
     </div>
